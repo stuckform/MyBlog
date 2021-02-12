ccc14ba6ba186cd7f72eb26e316e69f7697f4c00
stuckform <75707524+stuckform@users.noreply.github.com>
2021-02-12 06:11:46 -0500

    small model change

--- Models/CategoryPost.cs
+++ Models/CategoryPost.cs
@@ -12,7 +12,6 @@ namespace MyBlog.Models
         public int BlogCategoryId { get; set; }
 
         [Required]
-        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
         public string Title { get; set; }
 
         [Required]
